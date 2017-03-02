69fe9f2049eb62178f313c596e70c95e9de181f5
prathameshkesarkar <pratham.kearkar@gmail.com>
2017-03-03 02:47:03 +0530

    Add Update script

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -9,28 +9,41 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
         return console.log('Unable to connect to MongoDB server');
     }
     console.log('Connected to MongoDB server');
-    // db.collection('Todos')
-    // .findOneAndUpdate({_id:new ObjectID("589a2760a73d4b0903fab66f")},{
-    //   $set:{
-    //     completed:true
-    //   }
-    // },{
-    //   returnOriginal:false
+
+
+//  Update the Todos completed status
+    // db.collection('Todos').findOneAndUpdate({
+    //   _id:new ObjectID("58b8865d485f00cc27082c99")
+    //   },{
+    //     $set:{
+    //       completed:true
+    //     }
+    //   },{
+    //     returnOriginal:false
     // })
     // .then((result)=>{
-    //   console.log(result);
+    //     console.log(result);
+    //   },(err)=>{
+    //     console.log(err);
     // });
 
+//Update the Users name and increment its age
+db.collection('Users').findOneAndUpdate({
+  _id:123
+},{
+  $set:{
+    name:'Prathamesh'
+  },
+  $inc:{
+    age:1
+  }
+},{
+  returnOriginal:false
+}).then((result)=>{
+  console.log(JSON.stringify(result,undefined,2));
+},(err)=>{
+  console.log(err);
+});
 
-    db.collection('Users').findOneAndUpdate({_id:new ObjectID('5899b2446f48b839ea6895b8')},{
-      $set:{
-        name:'Prathamesh'
-      },$inc:{
-          age:1
-      }
-    },{
-      returnOriginal:false
-    }).then((result)=>{
-      console.log(JSON.stringify(result,undefined,2));
-    })
+    db.close();
 });
