3d639d8f30e3f601325331f948b5245dee8ca0d0
prathameshkesarkar <pratham.kearkar@gmail.com>
2017-03-18 14:22:47 +0530

    ADD GET /users/me and authentication middleware

---
+++ playground/hashing.js
@@ -0,0 +1,21 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+  id:10
+};
+
+
+var token = jwt.sign(data,'123abc');
+console.log(token);
+
+var decode =  jwt.verify(token,'123abc');
+
+console.log('decoded',decode);
+
+
+// var message = "I am user number 3";
+// var hash = SHA256(message).toString();
+//
+// console.log('Message:',message);
+// console.log('Hash:',hash);
